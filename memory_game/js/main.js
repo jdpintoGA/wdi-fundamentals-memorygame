baabea0429d1f4543689a4629bfbed8438c8677d
joao <jdpintoGA@users.noreply.github.com>
2020-01-21 19:31:24 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,39 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
 let cardsInPlay = [];
+
 
 function checkForMatch () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("you found a match!");
 		} else {
-			console.log("try again :(");
+			alert("try again :(");
 		}
 };
 
 function flipCard(cardId) {
-console.log("user flipped "+ cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+console.log("user flipped "+ cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
 		if (cardsInPlay.length === 2) {
 			checkForMatch()
 		}
@@ -20,4 +42,10 @@ console.log("user flipped "+ cards[cardId]);
 
 
 flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
+
 flipCard(2);
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
+
