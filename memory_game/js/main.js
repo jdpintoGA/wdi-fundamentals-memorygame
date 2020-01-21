d7bfca1400a603734f45705f951f0b6fe51d5155
joao <jdpintoGA@users.noreply.github.com>
2020-01-21 18:06:53 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log("user has flipped "+ cardOne);
-console.log("user has flipped "+ cardThree);
+let cardOne = "queen";
+let cardTwo = "king";
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) { 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
+	} else {
+		alert("try again :(");
+	}
+
+};
+
+
+
+
+
+
+
+
+console.log("user flipped "+ cardOne);
+console.log("user flipped "+ cardTwo);
+
+ 
